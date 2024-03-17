99b6fe7b79b26caa66a8cd9573dc4a1a286d5787
Eddy <volkikushaytnogi@gmail.com>
2024-03-17 13:21:55 +0100

    started
    -

---
+++ Contact.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module15LINQ2
+{
+    internal class Contact(string name, long phone, string email)
+    {
+        public string Name { get; set; } = name;
+        public long Phone { get; set; } = phone;
+
+        public string Email { get; set; } = email;
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Module15LINQ2
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var phoneBook = new List<Contact>
+            {
+                // добавляем контакты
+                new("Игорь", 79990000001, "igor@example.com"),
+                new("Сергей", 79990000010, "serge@example.com"),
+                new("Анатолий", 79990000011, "anatoly@example.com"),
+                new("Валерий", 79990000012, "valera@example.com"),
+                new("Сергей", 799900000013, "serg@gmail.com"),
+                new("Иннокентий", 799900000013, "innokentii@example.com")
+            };
+
+            var contactsByEmail = from s in phoneBook
+                group s by s.Email into grouping
+                select new
+                    {
+                        Name = grouping.Key,
+                        Cars = from p in grouping select p //  выполним подзапрос, чтобы заполнить список машин внутри нашего нового типа
+                    };
+
+        }
+    }
+}
